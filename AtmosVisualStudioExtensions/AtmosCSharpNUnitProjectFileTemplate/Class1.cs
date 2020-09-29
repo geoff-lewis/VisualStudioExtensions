29383a6b81217815025582060cbe5a1aaa65a07d
Geoff.Lewis <Geoff.Lewis@atmosi.com>
2020-09-29 14:11:03 +0100

    V1.1 With templates for C# Class Library and C# NUnit

---
+++ AtmosVisualStudioExtensions/AtmosCSharpNUnitProjectFileTemplate/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+using NUnit.Framework;
+
+namespace $safeprojectname$
+{
+	[TestFixture()]
+	public class Class1
+{
+}
+}
